54071a566c5f97da2b0a3c1332bd955e9e294f8a
Jeferson350 <jefersonfernandes350@gmail.com>
2022-08-22 10:24:14 -0300

    nnnn

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-console.log('olá mundo')
+const express = require('express');
+ 
+const app = express();
+ 
+app.get('/home', (req, res) => {
+    res.send('Olá Turma. Nossa primeira API!');
+});
+ 
+app.listen(3333);
